fc93baa420716ab65efb4f80665dca3adb623953
Anshul Kothari <anshulkothari96@gmail.com>
2018-02-19 10:43:11 +0530

    changes made to make table responsive using bootstrap

---
+++ Repo_Table/js/script.js
@@ -0,0 +1,56 @@
+var content = "";
+var repo_array;
+function display_JSON(){
+	var table = document.getElementById("json_table");
+	str = "";
+	readTextFile();
+	
+	repo_array = JSON.parse(content);
+	for (i in repo_array){
+		str += "<tr>";
+
+		for (key in repo_array[i]){
+			str += "<td>" + repo_array[i][key] + "</td>";
+			
+		}
+
+		str += "</tr>";
+		
+	}
+	table.innerHTML += str;
+	
+	
+}
+function readTextFile()
+{	var file = "repo_data.json";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {	 content = rawFile.responseText;
+       
+    }
+    rawFile.send(null);
+}
+
+function search(){
+	var url = document.getElementById("url").value;
+	var result = document.getElementById("result");
+	var str = "";
+	if(isValid(url) == false){
+		result.innerHTML= "Please Enter a Valid URL to Search ";
+	}
+	else{
+		for(i in repo_array){
+			if(url == repo_array[i]["url"]){
+				for(keys in repo_array[i]){
+					str += "  " + repo_array[i][keys]  + "  "; 
+				}
+			}
+		}
+		result.innerHTML = str;
+	}
+}
+function isValid(url){
+	return /https:\/\/github.com\/[a-zA-Z]+\/[a-zA-Z]+/.test(url) ;
+	
+}
